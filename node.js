ef16698e8a08e7762612e9cfe3693e696614d97e
Bilal-12-2 <91077424+Bilal-12-2@users.noreply.github.com>
2021-12-24 15:24:38 +0500

    Learn JS

---
+++ node.js
@@ -0,0 +1,45 @@
+// let months = ['Jan', 'Feb' , 'March', 'April'];
+// // const indexOfMonths = months.indexOf('Feb');
+
+// months.splice(2,-1);
+// if(indexOfMonths != -1){
+//     const updateMonths = months.splice(indexOfMonths,1);
+//     console.log('We do it');
+// }
+// else{
+//     console.log('Better luck next time');
+// }
+
+// console.log(months);
+
+
+// const best = [1,36,7,8,9,7,4,2,65,7,9,7,464,2,4,8,9,79,7,46,45,769809,8,57,6,87,9,85,7];
+// const nobody = best.indexOf(7);
+// console.log(nobody);
+
+// let provinces = ['Punjab', 'KPK', 'Balochistan', 'Sindh'];
+// let belongTo = provinces.map((curelm, index, arr) => {
+// return `The index is ${index} of the ${curelm} is the Province of Pakistan`;
+// })
+
+// console.log(belongTo);
+
+
+// let numbers = [9,16,81,121];
+// let squareNumbers = numbers.map((curelm)=>
+// {
+//     return Math.sqrt(curelm);
+// })
+
+// console.log(squareNumbers);
+
+
+let numbers = [1,4,7,9,24,78,99];
+let multiNumbers = numbers.map((curElm) => {
+    return curElm * 2;
+}).filter((curElm) => {
+    return curElm > 10;
+
+})
+
+console.log(multiNumbers)
