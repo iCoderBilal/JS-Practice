7929ce532e452037c7c984344c9566372c79820f
Bilal-12-2 <91077424+Bilal-12-2@users.noreply.github.com>
2021-12-27 15:40:56 +0500

    jfir

--- node.js
+++ node.js
@@ -66,6 +66,104 @@
 // let myName = 'Mian Muhammad Bilal';
 // console.log(myName.length);
 
-let myName = 'My name is "Mian Muhammad Bilal".'
-const indName = myName.lastIndexOf('M', 18);
-console.log(indName);
+// let myName = 'My name is "Mian Muhammad Bilal".'
+// const indName = myName.lastIndexOf('Muhammad');
+// console.log(indName);
+
+
+
+// let myName = 'Mian Muhammad Bilal';
+// console.log(myName.search('Muhammad'));
+// let numbers = [1 ,4 ,6, 9, 10];
+// let then = numbers.push(11);
+// console.log(numbers);
+
+
+
+
+// let number = 110;
+// let formula = number%2;
+
+// if(formula == 0){
+//     console.log('This is even number')
+// }else{
+//     console.log('This is odd Number')
+// }
+
+
+
+// let myNewArray = [
+//     [1 , 2],
+//     [3 ,4 ],
+//     [5 , 6]
+// ]
+
+// let newSum = myNewArray.reduce((accum, currValue)=>{
+//     return accum.concat(currValue);
+// })
+
+// console.log(newSum);
+
+
+// let theNumbers = 'Mian Muhammad Bilal';
+// let newNumber = theNumbers.substr(-3);
+// console.log(newNumber);
+
+
+// Replacing the String Content
+// let name = 'Mian muhammad Bilal';
+// let newName = name.replace('muhammad', 'Muhammad');
+// console.log(newName);
+
+// charAt Method
+// let name = 'Mian muhammad Bilal';
+// console.log(name.charAt(7));
+
+
+
+// charCodeAt Method to find the unicode of Character. Unicode (0 to 65535)
+// let myName = 'Mian';
+// console.log(myName.charCodeAt(lastChar));
+
+
+
+// Upper case and lower case method 
+
+// let myName = 'Mian Muhammad Bilal';
+// console.log(myName.toUpperCase());
+
+
+// let myName = 'Mian Muhammad';
+// let fullName = 'Bilal';
+// console.log(myName.concat(' ', fullName));
+
+// let myName = '                      Bilal                   ';
+// console.log(myName.trim());
+
+
+// let numbers = '1,2,4,6,7,8';
+// console.log(numbers.split());
+
+// let currDate = new Date();
+// console.log(currDate.toString());
+// console.log(currDate.toLocaleString());
+
+// console.log(Date.now()); it gives data in milliseconds since 1970.
+
+// let tareekh = new Date(2021,11,27);
+// console.log(tareekh.toLocaleString());
+
+// let tareekh = new Date('December 27, 2021 12:12:13' );
+// console.log(tareekh.toLocaleString());
+
+let tareekh = new Date(767365746756487587485);
+console.log(tareekh.toLocaleString());
+
+
+    
+    
+
+
+
+
+
